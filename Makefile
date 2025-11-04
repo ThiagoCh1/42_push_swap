b654a9dab52ff02f8d7c4f3f397b991d5759ad46
thribeir <thiagoribeirochico@gmail.com>
2025-11-04 19:22:58 +0000

    implemented index

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@
 NAME        = push_swap
 
 CC          = cc
-CFLAGS      = -Wall -Wextra -Werror
+CFLAGS      = -Wall -Wextra -Werror -g
 
 SRC_DIR     = src
 LIBFT_DIR   = 42_libft
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,19 +6,45 @@
 /*   By: thribeir <thribeir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 20:19:37 by thribeir          #+#    #+#             */
-/*   Updated: 2025/11/03 21:49:41 by thribeir         ###   ########.fr       */
+/*   Updated: 2025/11/04 18:59:13 by thribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
+int	*sorted_arr(char **not_sorted, int len)
+{
+	int		i;
+	int		j;
+	int		*sorted;
+	int		count;
+	
+	sorted = malloc(len * sizeof(int));
+	i = 0;
+	while (not_sorted[i])
+	{
+		j = 0;
+		count = 0;
+		while (not_sorted[j])
+		{
+			if (ft_atoi(not_sorted[i]) > ft_atoi(not_sorted[j]))
+				count++;
+			j++;
+		}
+		sorted[count] = ft_atoi(not_sorted[i]);
+		i++;
+	}
+	sorted[i] = '\0';
+	return (sorted);
+}
+
 int main(int argc, char **argv)
 {
 	char	**num_arr;
-	t_list	*a;
-	t_list	*b;
+	//t_list	*a;
 	int		i;
+	int		*sorted;
 
 	i = 0;
 	if (argc == 2)
@@ -26,7 +52,8 @@ int main(int argc, char **argv)
 		num_arr = ft_split(argv[1], ' ');
 		while (num_arr[i])
 			i++;
-		a = ft_calloc(i, sizeof(t_list));
-		printf("%s, %s, %s", num_arr[0], num_arr[1], num_arr[2]);
+		//a = ft_calloc(i, sizeof(t_list));
+		sorted = sorted_arr(num_arr, i + 1);
+		printf("%d, %d, %d", sorted[0], sorted[1], sorted[2]);
 	}
 }
